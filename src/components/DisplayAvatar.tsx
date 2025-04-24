2730a81741178a6bec82015cc547a6fa2c3823f6
amelia-liston <aliston@bu.edu>
2025-04-24 19:53:43 -0400

    Created new component to display the avatars

--- src/components/DisplayAvatar.tsx
+++ src/components/DisplayAvatar.tsx
@@ -7,16 +7,26 @@ type AvatarProps = {
 }
 
 const StyledImage = styled.img`
-    max-width: 10%;
+    max-width: 80%;
     margin: 1vh 1vw;
+`;
+
+const OneAvatarDiv=styled.div`
+    flex-direction: column;
+    justify-content: center;
+    max-width: 100%;
+    text-align: center;
 `;
 
 export default function DisplayAvatar({ name, img, bio }: AvatarProps) {
     return(
         <>
-            <StyledImage src={img} alt={`${name}'s Avatar`} />
-            <h3>{name}</h3>
-            <h4>{bio}</h4>
+            <OneAvatarDiv>
+                <StyledImage src={img} alt={`${name}'s Avatar`} />
+                <h3>{name}</h3>
+                <h4>{bio}</h4>
+            </OneAvatarDiv>
+
         </>
     )
 }
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,6 +1,12 @@
 import DisplayAll from "../DisplayAll.tsx";
 import DisplayAvatar from "../DisplayAvatar.tsx";
+import styled from "styled-components";
 
+const AllAvatarsDiv=styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+`;
 
 export default function About() {
     return(
@@ -10,10 +16,12 @@ export default function About() {
                 <h2>We're just some CS girlies curious about which mob we might be. We made these mob cards for the final results of the quiz. Take our quiz to find out which mob you are!</h2>
                 <DisplayAll/>
                 <h2>About the Creators</h2>
-                <DisplayAvatar img="/amelia-avatar.png" name="Amelia" bio="CS girly pop"/>
-                <DisplayAvatar img="/mia-avatar.png" name="Mia" bio="CS girly pop"/>
-                <DisplayAvatar img="/kris-avatar.png" name="Kris" bio="CS girly pop"/>
-                <DisplayAvatar img="/sophia-avatar.png" name="Sophia" bio="CS girly pop"/>
+                <AllAvatarsDiv>
+                    <DisplayAvatar img="/amelia-avatar.png" name="Amelia" bio="CS girly pop"/>
+                    <DisplayAvatar img="/mia-avatar.png" name="Mia" bio="CS girly pop"/>
+                    <DisplayAvatar img="/kris-avatar.png" name="Kris" bio="CS girly pop"/>
+                    <DisplayAvatar img="/sophia-avatar.png" name="Sophia" bio="CS girly pop"/>
+                </AllAvatarsDiv>
             </main>
         </>
     )
