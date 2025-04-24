b8f5caafe0a9b3520fc69666fe668d6299bd132c
rkkp25 <rkkp@bu.edu>
2025-04-24 17:37:37 -0400

    added HomePreview.tsx, QuizDisplay.tsx, and edited App.tsx to use Routers

---
+++ src/components/pages/HomePreview.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router";
+
+export default function HomePreview() {
+
+    return (
+      <div>
+        <h1>Welcome to the Minecraft personality quiz!</h1>
+        <h3>Press START to begin the quiz!</h3>
+        <Link to={`/QuizDisplay`}>START</Link>
+      </div>
+    )
+  }
