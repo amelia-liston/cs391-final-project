ed602b2766976576ac58af16239ecc372d7792fa
amelia-liston <aliston@bu.edu>
2025-04-24 17:22:30 -0400

    Added content to about page

---
+++ src/components/pages/About.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return(
+        <>
+            <main>
+                <h1>About This Game</h1>
+                <h2></h2>
+            </main>
+        </>
+    )
+}
