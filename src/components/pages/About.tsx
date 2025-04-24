16c5260d609b95bbaf8a43db84bf9b7b439220b6
amelia-liston <aliston@bu.edu>
2025-04-24 19:32:38 -0400

    Added all avatar pngs

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,10 +1,22 @@
 import DisplayAll from "../DisplayAll.tsx";
+import styled from "styled-components";
+
+const StyledImage = styled.img`
+    max-width: 30%;
+    margin: 1vh 1vw;
+`;
+
 export default function About() {
     return(
         <>
             <main>
                 <h1>About This Game</h1>
+                <h2>We're just some CS girlies curious about which mob we might be. We made these mob cards for the final results of the quiz. Take our quiz to find out which mob you are!</h2>
                 <DisplayAll/>
+                <StyledImage src={"/amelia-avatar.png"} alt="Amelia's Avatar"/>
+                <StyledImage src={"/mia-avatar.png"} alt="Mia's Avatar"/>
+                <StyledImage src={"/kris-avatar.png"} alt="Kris's Avatar"/>
+                <StyledImage src={"/sophia-avatar.png"} alt="Sophia's Avatar"/>
             </main>
         </>
     )
