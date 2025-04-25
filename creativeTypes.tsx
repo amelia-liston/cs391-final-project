ab0aab1153638b14f871810bc44f9ba68d352485
Show530 <sophiahowson@gmail.com>
2025-04-24 23:51:49 -0400

    added preliminary creative question json and creative type files

---
+++ creativeTypes.tsx
@@ -0,0 +1,20 @@
+export type CreativeQuestions= {
+    id: string;
+    question: string;
+    // a1
+    a1: string | null;
+    a1Type: string | null;
+    a1nextId: string | null;
+    // a2
+    a2: string | null;
+    a2Type: string;
+    a2nextId: string | null;
+    // a3
+    a3: string | null;
+    a3Type: string;
+    a3nextId: string | null;
+    // a4
+    a4: string | null;
+    a4Type: string | null;
+    a4nextId: string | null;
+}
