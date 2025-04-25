efcbb96f831c77d4080c55f8fe2e2e0f59ac6dae
rkkp25 <rkkp@bu.edu>
2025-04-25 17:41:01 -0400

    added yellow text

--- src/components/pages/HomePreview.tsx
+++ src/components/pages/HomePreview.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import styled from "styled-components"
 
@@ -10,12 +11,44 @@ const StyledImg = styled.img`
     margin-bottom: 2vw;
 `;
 
+const StyledYellowText = styled.img`
+    max-width: 20%;
+    position: absolute;
+    top: 35%;
+    left: 63%;
+    rotate: -15deg;
+    animation: grow-animation 4s infinite alternate;
+
+}
+
+`;
+
 
 export default function HomePreview() {
+    const [text, setText] = useState('yellowText/1.svg');
+    const yellowText = ['yellowText/1.svg', 'yellowText/2.svg', 'yellowText/3.svg', 'yellowText/4.svg', 'yellowText/5.svg', 'yellowText/6.svg', 'yellowText/7.svg', 'yellowText/8.svg', 'yellowText/9.svg', 'yellowText/10.svg', 'yellowText/11.svg', 'yellowText/12.svg', 'yellowText/13.svg'];
+
+    useEffect(() => {
+        try {
+            setText(() => yellowText[Math.floor(Math.random() * yellowText.length)]);
+            // console.log(yellowText.length)
+            console.log(Math.random() * yellowText.length)
+            console.log(yellowText[Math.floor(Math.random() * yellowText.length)]);
+            console.log(text);
+    
+        }
+        catch {
+            setText('yellowText/1.svg')
+        }
+        }, []
+    );
+
+    
 
     return (
       <StyledDiv>
         <StyledImg src="minecraft-logo-3.svg" alt="minecraft logo"></StyledImg><br></br>
+        <StyledYellowText src={text} alt={text} />
         <Link className="minecraft-button" to={`/QuizStart`}>Play</Link>
         {/* <button className="minecraft-btn mx-auto w-64 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Click me!</button> */}
 
