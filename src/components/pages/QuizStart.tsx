daaef1dcfe0d139f6d32dc75268f0b17db45f620
Show530 <sophiahowson@gmail.com>
2025-04-27 14:42:46 -0400

    added survival json and survival quiz handling!

--- src/components/pages/QuizStart.tsx
+++ src/components/pages/QuizStart.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function QuizStart() {
     return (
         <div>
-            <h1 className="minecraft-btn">Survival</h1>
+            <Link className="minecraft-btn" to={`/SurvivalQuiz`}>Survival</Link>
             {/* <h1 className="minecraft-btn">Creative</h1> */}
             <Link className="minecraft-btn" to={`/CreativeQuiz`}>Creative</Link>
         </div>
