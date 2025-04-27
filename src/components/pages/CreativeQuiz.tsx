ecbc3544ef9a7ea74f5baea8d5ddd9ce4f021df2
Show530 <sophiahowson@gmail.com>
2025-04-27 14:01:18 -0400

    finised creative json (maybr bugs but seems good), added handloing for end of quiz

--- src/components/pages/CreativeQuiz.tsx
+++ src/components/pages/CreativeQuiz.tsx
@@ -44,7 +44,7 @@ export default function CreativeQuiz() {
         fetching();
     }, []);
 
-    // We have no data yet
+    // There is no data loaded yet
     if (data === null){
         console.log("Still setting data/loading!");
         // show loading component here?
@@ -55,57 +55,53 @@ export default function CreativeQuiz() {
             </>
         );
     }
-    // data exists!
-    //maybe get rid of else? -mia
+    // data exists!- can do quiz!
     else {
         console.log("Data here!", data);
-        // fully loaded data
-        // console.log("Question: ", getQuestion(data));
+
         const currQuestion = getQuestion(data, currId);
-        if (!currQuestion){
-            return <h2>"Current question not found!"</h2>;
-        }
-            //mia suggestion, no idea if itll work HEHE
+        // error checking here!
+
+        // if the question exists, build the display
+        if (currQuestion) {
             //building an options array 
-            //ensure if there are less than 4 options it will be ok on the next component!
-            //
+            // this ensures if there are less than 4 options it will be ok on the next component!
             const options: Option[] = []
             if (currQuestion.a1 && currQuestion.a1nextId) {
                 options.push({
-                  text: currQuestion.a1,
-                  nextId: currQuestion.a1nextId,
-                  type: currQuestion.a1Type,
+                    text: currQuestion.a1,
+                    nextId: currQuestion.a1nextId,
+                    type: currQuestion.a1Type,
                 })
-              }
-              if (currQuestion.a2 && currQuestion.a2nextId) {
+            }
+            if (currQuestion.a2 && currQuestion.a2nextId) {
                 options.push({
-                  text: currQuestion.a2,
-                  nextId: currQuestion.a2nextId,
-                  type: currQuestion.a2Type,
+                    text: currQuestion.a2,
+                    nextId: currQuestion.a2nextId,
+                    type: currQuestion.a2Type,
                 })
-              }
-              if (currQuestion.a3 && currQuestion.a3nextId) {
+            }
+            if (currQuestion.a3 && currQuestion.a3nextId) {
                 options.push({
-                  text: currQuestion.a3,
-                  nextId: currQuestion.a3nextId,
-                  type: currQuestion.a3Type,
+                    text: currQuestion.a3,
+                    nextId: currQuestion.a3nextId,
+                    type: currQuestion.a3Type,
                 })
-              }
-              if (currQuestion.a4 && currQuestion.a4nextId) {
+            }
+            if (currQuestion.a4 && currQuestion.a4nextId) {
                 options.push({
-                  text: currQuestion.a4,
-                  nextId: currQuestion.a4nextId,
-                  type: currQuestion.a4Type,
+                    text: currQuestion.a4,
+                    nextId: currQuestion.a4nextId,
+                    type: currQuestion.a4Type,
                 })
-              }
-            
-              //go to next question
-              //makes the quiz show the next question
-              //i feel like here will handle the incrementing of the personalities!
+            }
+
+            //go to next question
+            //makes the quiz show the next question
             const handleSelect = (idx: number) => {
                 const opt = options [idx]
                 if (!opt) return
-                    switch (opt.type) {
+                switch (opt.type) {
                     case 'extraversion':
                         setExtraversion((v) => v + 1)
                         break
@@ -133,32 +129,34 @@ export default function CreativeQuiz() {
                     default:
                         //not type so nothing incrememented :)
                         break
-                    }
-                    setCurrId(opt.nextId)
                 }
-        // Finished quiz!
-        if (currId === "FINISHED"){
-            return (
-                <h2>Finished!</h2>
-            );
-        }
-        // Still doing quiz
-        else {
+                setCurrId(opt.nextId)
+            }
+
             return (
                 // display component -mia
                 // pass in the questions and answers as props into the function -mia
-                //handle select of what the player chooses
+                // handle select of what the player chooses
                 <>
-                    {/* <h2>There is data!</h2> */}
                     <DisplayQuestion
 
                         question={currQuestion.question}
                         //option array passed in
-                        options = {options.map(o => o.text)}
+                        options={options.map(o => o.text)}
                         onSelect={handleSelect}
                     />
                 </>
             );
+        }
+        // quiz is finished, load their character that they've earned!
+        else if (currId === "FINISHED") {
+            return (
+                <h2>Finished quiz</h2>
+            );
+        }
+        // No question here; error checking
+        else {
+            return <h2>"Current question not found!"</h2>;
         }
     }
 
