07a793adaaef108ae48401032b992fa00b9a1ec0
rkkp25 <rkkp@bu.edu>
2025-04-30 14:18:39 -0400

    added a QR code page, editted Home page and App.tsx to route to that page, and used the nav bar so we can get back to the home page from the QR code page.

--- src/components/pages/HomePreview.tsx
+++ src/components/pages/HomePreview.tsx
@@ -72,6 +72,7 @@ export default function HomePreview() {
                 <StyledImg src="minecraft-logo-3.svg" alt="minecraft logo"></StyledImg><br></br>
                 <StyledYellowText src={text} alt={text} />
                 <Link className="minecraft-button" to={`/QuizStart`}>Play</Link>
+                <Link className="minecraft-button" to={`/QRCode`}>Play Along</Link>
                 {/* <button className="minecraft-btn mx-auto w-64 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Click me!</button> */}
                 <Link className="minecraft-button" to={`/About`}>About</Link>
             </StyledDiv>
